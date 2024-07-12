dd9c3a8303eaaceafbea06fc8cc34d297521b949
ajay-2611 <pappuriajaykumar7@gmail.com>
2024-07-12 18:26:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const entriesRoutes = require('./routes/entries');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Ajay-26:Ajay-26@cluster2.k3xpcnj.mongodb.net/test?authMechanism=DEFAULT', {
+mongoose.connect('mongodb+srv://Ajay-26:<password>@cluster2.k3xpcnj.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster2', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -17,7 +17,10 @@ mongoose.connect('mongodb+srv://Ajay-26:Ajay-26@cluster2.k3xpcnj.mongodb.net/tes
     console.error('Error connecting to MongoDB', err);
 });
 
-app.use(cors());
+
+ app.use(cors({
+  origin: 'https://vercel.com/new/pappuri-ajaykumars-projects/'
+}));
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
